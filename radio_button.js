536f52ec370b5cc3292ea0bd97b456dd64f59053
James Padgett <jampad14@gmail.com>
2018-12-09 22:01:14 -0500

    I finally made the boxes work

--- radio_button.js
+++ radio_button.js
@@ -1,6 +1,27 @@
-function onlyOne(checkbox) {
-    var checkboxes = document.getElementsByName('check')
-    checkboxes.forEach((item) => {
-        if (item !== checkbox) item.checked = false
-    })
+// function onlyOne(checkbox) {
+//     var checkboxes = document.getElementsByName('check')
+//     checkboxes.forEach((item) => {
+//         if (item !== checkbox) item.checked = false
+//     })
+// }
+
+function ckChange(ckType){
+    var ckName = document.getElementsByName(ckType.name);
+    var checked = document.getElementById(ckType.id);
+
+    if (checked.checked) {
+      for(var i=0; i < ckName.length; i++){
+
+          if(!ckName[i].checked){
+              ckName[i].disabled = true;
+          }else{
+              ckName[i].disabled = false;
+          }
+      } 
+    }
+    else {
+      for(var i=0; i < ckName.length; i++){
+        ckName[i].disabled = false;
+      } 
+    }    
 }
